0cd530fd45bab920942862d034818d8ae2b35e75
Lucas Vilas Boas Santos <vilasboaslucasm@gmail.com>
2023-05-20 23:16:36 -0300

    refactoring url

--- movie/urls.py
+++ movie/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from movie import views
 
 urlpatterns = [
-    path('movies/', views.MovieList.as_view(), name='movie-list'),
-    path('movies/<int:pk>', views.MovieDetail.as_view(), name='movie-detail'),
+    path('', views.MovieList.as_view(), name='movie-list'),
+    path('<int:pk>', views.MovieDetail.as_view(), name='movie-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
